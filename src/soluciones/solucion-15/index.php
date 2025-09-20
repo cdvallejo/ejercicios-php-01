e9b63830ee17cefc7094657314b384b8aede9102
cdvallejo <carlosd.vallejo@gmail.com>
2025-09-20 14:38:21 +0200

    dibujando tabla ejercicio 15

--- src/soluciones/solucion-15/index.php
+++ src/soluciones/solucion-15/index.php
@@ -102,7 +102,7 @@
                     <td class="blanco"></td>
                     <td class="negro"></td>
                     <td class="blanco"></td>
-                   
+
                     <td class="negro"></td>
                     <td class="blanco"></td>
                     <td class="negro"></td>
@@ -117,26 +117,26 @@
                     <td class="negro"></td>
                     <td class="blanco"></td>
                     <td class="negro"></td>
-                 
+
                     <td class="blanco"></td>
                     <td class="margen-coordenadas">3</td>
                 </tr>
                 <tr>
                     <td class="margen-coordenadas">2</td>
-                    <td class ="blanco"></td>
-                   
-                    <td class="negro"></td>
-                    
                     <td class="blanco"></td>
-                  
+
                     <td class="negro"></td>
-                
+
                     <td class="blanco"></td>
-               
+
                     <td class="negro"></td>
-                
+
                     <td class="blanco"></td>
-         
+
+                    <td class="negro"></td>
+
+                    <td class="blanco"></td>
+
                     <td class="negro"></td>
                     <td class="margen-coordenadas">2</td>
                 </tr>
@@ -165,7 +165,7 @@
                     <td></td>
                 </tr>
             </table>
-            
+
             <form action="index.php" method="get">
                 <label for="coordenada"> Introduzca las coordenadas del alfil (ejemplo: e4)</label>
                 <br>
@@ -179,50 +179,52 @@
                 echo "<br>";
 
                 $color = "";
-                ?>
+            ?>
                 <table>
-                <tr class="margen-coordenadas">
-                    <td></td>
-                    <td>a</td>
-                    <td>b</td>
-                    <td>c</td>
-                    <td>d</td>
-                    <td>e</td>
-                    <td>f</td>
-                    <td>g</td>
-                    <td>h</td>
-                    <td></td>
-                </tr>
-                <tr>
-                <?php
-                for ($fila = 8; $fila > 0; $fila--) {
-                    echo "<tr>";
-                    echo "<td class='margen-coordenadas'>$fila</td>";
-                    for ($col = 9; $col > 0; $col--) {
-                        if ($col / 2 != 0) {
-                            $color = "negro";
-                        } else {
-                            $color = "blanco";
+                    <tr class="margen-coordenadas">
+                        <td></td>
+                        <td>a</td>
+                        <td>b</td>
+                        <td>c</td>
+                        <td>d</td>
+                        <td>e</td>
+                        <td>f</td>
+                        <td>g</td>
+                        <td>h</td>
+                        <td></td>
+                    </tr>
+                    <tr>
+                        <?php
+                        for ($fila = 8; $fila > 0; $fila--) {
+                            echo "<tr>";
+                            echo "<td class='margen-coordenadas'>$fila</td>";
+                            for ($col = 8; $col > 0; $col--) {
+                                if ($col / 2 != 0) {
+                                    $color = "negro";
+                                    echo "<td class=$color></td>";
+                                } else {
+                                    $color = "blanco";
+                                    echo "<td class=$color></td>";
+                                }
+                            }
+                            echo "<td class='margen-coordenadas'>$fila</td>";
+                            echo "</tr>";
                         }
-                        echo "<td class='margen-coordenadas'>$fila</td>";
-                    }
-                    echo "</tr>";
-                }
-                ?>
-                <tr class="margen-coordenadas">
-                    <td></td>
-                    <td>a</td>
-                    <td>b</td>
-                    <td>c</td>
-                    <td>d</td>
-                    <td>e</td>
-                    <td>f</td>
-                    <td>g</td>
-                    <td>h</td>
-                    <td></td>
-                </tr>
+                        ?>
+                    <tr class="margen-coordenadas">
+                        <td></td>
+                        <td>a</td>
+                        <td>b</td>
+                        <td>c</td>
+                        <td>d</td>
+                        <td>e</td>
+                        <td>f</td>
+                        <td>g</td>
+                        <td>h</td>
+                        <td></td>
+                    </tr>
                 </table>
-                <?php
+            <?php
             }
             ?>
         </div>
